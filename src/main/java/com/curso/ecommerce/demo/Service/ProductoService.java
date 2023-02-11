5857a0ad3a3eac57e08357ef8ba4dd26fe66b5f0
DerianAguilar <116276342+DerianAguilar@users.noreply.github.com>
2023-02-11 13:50:16 -0500

    Funcinaidad mostrar productos para el administrador

--- src/main/java/com/curso/ecommerce/demo/Service/ProductoService.java
+++ src/main/java/com/curso/ecommerce/demo/Service/ProductoService.java
@@ -2,6 +2,7 @@ package com.curso.ecommerce.demo.Service;
 
 import com.curso.ecommerce.demo.Model.Producto;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ProductoService {
@@ -12,4 +13,6 @@ public interface ProductoService {
     public void update(Producto producto);
 
     public void delete(Integer id);
+
+    public List<Producto> findAll();
 }
